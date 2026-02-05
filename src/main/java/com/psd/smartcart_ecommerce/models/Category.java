73b778affe48195577186fe20fa5e19c8d92ea32
mathew <mashereni@gmail.com>
2026-02-04 22:26:32 -0600

    Category model and controller <createCategory & getAllCategory>

---
+++ src/main/java/com/psd/smartcart_ecommerce/models/Category.java
@@ -0,0 +1,30 @@
+package com.psd.smartcart_ecommerce.models;
+
+
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
+
